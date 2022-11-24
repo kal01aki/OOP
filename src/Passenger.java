78ce1ed348bd915ba5ce930845fdfb7f9ace2df7
kal01aki <kal01aki@users.noreply.github.com>
2022-11-24 15:06:37 +0300

    Initial commit

---
+++ src/Passenger.java
@@ -0,0 +1,10 @@
+public class Passanger {
+    String passangerList [];
+    int capacity;
+    String modeOfTransport[] = String ("Bus","Ship","Plane")
+
+            void addPassanger(int id, String passangerName){
+        passangerList[id] = passangerName;
+            }
+
+}
