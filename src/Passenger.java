da8b12e79ab747c533b6b0cc3c8a9be57a94798d
kal01aki <kal01aki@users.noreply.github.com>
2022-11-24 15:08:32 +0300

    passenger

--- src/Passenger.java
+++ src/Passenger.java
@@ -1,10 +1,36 @@
-public class Passanger {
-    String passangerList [];
-    int capacity;
-    String modeOfTransport[] = String ("Bus","Ship","Plane")
+import java.util.Arrays;
 
-            void addPassanger(int id, String passangerName){
-        passangerList[id] = passangerName;
+public class Passenger {
+    String passengerList[];
+    int capacity;
+
+
+
+
+           void transportCapacity (){
+               passengerList = new String[capacity];
+           }
+            void addPassenger(int id, String passengerName){
+
+                passengerList[id] = passengerName;
+                System.out.println("Student added successfully");
             }
 
+            void viewTotalPassengers(){
+
+                System.out.println("Passenger list " +  Arrays.toString(passengerList));
+            }
+
+            void removePassenger (int id){
+
+                passengerList [id] = null;
+                System.out.println("Student removed successfully");
+            }
+            void updatePassenger (int id, String passengerName) {
+                passengerList[id] = passengerName;
+                System.out.println("Student updated successfully");
+            }
+            int totalStudent (){
+                return passengerList.length;
+            }
 }
